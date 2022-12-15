86b1371c4dbbf2f2fea191c7e523bb4fa3623a09
jnralexandre <alexandreoliveirajnr@gmail.com>
2022-12-15 12:44:33 -0300

    Add exercise 02

---
+++ javascript-html-desenvolva-um-jogo-e-pratique-logica-de-programacao/02-comunique-se-com-o-usuario/exercicio-02/script.js
@@ -0,0 +1,5 @@
+document.write("Agora vou utilizar alguns operadores aritméticos." + "<br>");
+document.write("A soma de 22 + 2 é: " + (22 + 2));
+document.write("<br>" + "A subtração de 301 - 9 é: " + (301 - 9));
+document.write("<br>" + "A multiplicação de 103 * 7 é: " + (103 * 7));
+document.write("<br>" + "A divisão de 10 / 2 é: " + (10 / 2));
